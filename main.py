343f12a7d93e16382e82a64ad301055d9b13af8c
Minejerik <jaanerikf@gmail.com>
2023-10-13 18:28:49 +0000

    almost done w/ first blog post about page and blog home page not done

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from random import choice
+import markdown
 
 app = Flask(__name__)
 
+# md = Markdown(app)
 
 #defines all possible titles
 titles = [
@@ -19,6 +21,29 @@ titles = [
 def index():
   #renders main html file w/ a random title extension
   return render_template("index.html", ran_title = choice(titles))
+
+@app.route('/blog/<id>')
+def blog_post(id):
+
+  try:
+    with open(f"blog/{id}.md", "r") as f:
+      mark = f.read()
+  except:
+    abort(404)
+
+  temp = mark.split("\n")
+  title = temp[0]
+  title = title.replace("TITLE:","")
+  source = temp[1]
+  source = source.replace("SOURCE:","")
+  content = temp[2:]
+  content = "\n".join(content)
+  content = content.replace(" .",".")
+  
+  return render_template("blog_post.html", 
+                         title = title, 
+                         content = markdown.markdown(content), 
+                         source=source)
 
 
 app.run(host='0.0.0.0', port=81, debug=True, use_evalex=False)
