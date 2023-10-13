cc2de95fb2063848a8b45f3cb517f4059594f359
Minejerik <jaanerikf@gmail.com>
2023-10-13 14:59:03 +0000

    addded index.html and working on random title extensions

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from random import choice
 
 app = Flask(__name__)
 
 
+#defines all possible titles
+titles = [
+  "Did you know these titles change? Reload the page for another!",
+  "HELLO FROM FLASK",
+  "I AM AN EPIC WEBSITE",
+  "I HATE JAVASCRIPT",
+  "NOW WITH LESS COLOR!",
+  "WHAT IS A DJANGO?",
+  "IS THE BEST WEBSITE EVER!"
+]
+
 @app.route('/')
 def index():
-    return 'Minejerik !'
+  #renders main html file w/ a random title extension
+  return render_template("index.html", ran_title = choice(titles))
 
 
 app.run(host='0.0.0.0', port=81)
